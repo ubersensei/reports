da036ab8be1e17ed6b26a10759eae7fcbf93a66f
Kaya <ubersensei@gmail.com>
2018-05-07 22:54:35 +0800

    made HBS work

---
+++ data/manipulate.js
@@ -0,0 +1,21 @@
+
+// for (let i = 60; i > -1; i-- ) {
+//     dates[i] = moment().subtract(i, 'days').format();
+// }
+
+// let baseline, tencent;
+// Object.keys(reports).map(id => {
+//     baseline = reports[id].baseline;
+//     tencent = Math.ceil(baseline * 0.1);
+//     outstandingItemsCounts[id] = [];
+//     dates.map(date => {
+//         outstandingItemsCounts[id].push({
+//             'date': date,
+//             'count':getRandomInt(0 - tencent, tencent) + baseline
+//         })
+//     });
+// });
+
+
+
+// document.write(JSON.stringify(outstandingItemsCounts));
